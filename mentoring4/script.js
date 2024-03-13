f1a5431e2201aef944b210edb9bcee9fce2b34c3
Marcin Pietrzyk <marcin.pietrzyk@vattenfall.com>
2024-03-13 13:13:13 +0100

    feat: 4th meeting task and set-up

---
+++ mentoring4/script.js
@@ -0,0 +1,6 @@
+(function handleCalculator() {
+  //When click on button
+  //Take the text value from the input field
+  //Perform calculation
+  //Display it somewhere (maybe in the input field itself)
+})()
